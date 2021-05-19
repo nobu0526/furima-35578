1b192e767d8a873e44e221a61081e422b16e820f
nobu0526 <nobu19930526@gmail.com>
2021-05-19 14:51:15 +0900

    password,nameに条件付きバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence:true
+
+  with_options presence:true, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: {with:/\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birthday, presence: true
 end

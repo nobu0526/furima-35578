38b1671a00cd0b3afbe5ee1b15ce37a11d532152
nobu0526 <nobu19930526@gmail.com>
2021-05-24 16:57:00 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    image      {}
-    
+    name               {"商品"}
+    description        {"説明"}
+    price              {1000}
+    category_id        {2}
+    status_id          {2}
+    charge_id          {2}
+    shipping_area_id   {2}
+    shipping_day_id    {2}
+    association :user
+
+  after(:build) do |item|
+    item.image.attach(io: File.open("spec/images/sample1.png"), filename: "sample1.png")
   end
+ end
+end

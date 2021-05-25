a73d80f87a4d5c0e3c90f09a18516ae814d891ec
nobu0526 <nobu19930526@gmail.com>
2021-05-25 17:32:59 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except:[:index, :show]
-  before_action :move_to_index,only:[:edit,:update]
   before_action :find_action,only:[:show, :edit, :update]
-
+  before_action :move_to_index,only:[:edit,:update]
+  
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -40,7 +40,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user == @item.user
   end
 
